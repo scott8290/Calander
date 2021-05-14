5db565307fcac55c9d0acd8e30fe73db73bfbf3b
pcy8290 <9766029@naver.com>
2021-05-14 19:24:17 +0900

    first

---
+++ app/src/main/java/com/example/calander/MonthView.java
@@ -0,0 +1,14 @@
+package com.example.calander;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MonthView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
